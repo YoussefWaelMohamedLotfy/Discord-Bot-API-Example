a4dfd10df47ae5fc5f92beaa4f5865b4e1c20580
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-05-14 00:42:55 +0200

    Added Moderation Module

---
+++ ASPNET-Discord-Bot/Modules/ModerationModule.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace ASPNET_Discord_Bot.Modules;
+
+public class ModerationModule : ModuleBase<SocketCommandContext>
+{
+    private readonly ILogger<ModerationModule> _logger;
+
+    public ModerationModule(ILogger<ModerationModule> logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    [Command("purge")]
+    [RequireUserPermission(GuildPermission.ManageMessages)]
+    public async Task Purge(int amount)
+    {
+        var messages = await Context.Channel.GetMessagesAsync(amount + 1).FlattenAsync();
+        await (Context.Channel as SocketTextChannel)!.DeleteMessagesAsync(messages);
+
+        var message = await ReplyAsync($"{messages.Count()} messages deleted Successfully!");
+        await Task.Delay(2500);
+        await message.DeleteAsync();
+
+        _logger.LogInformation($"{Context.User.Username} executed the purge command!");
+    }
+}
